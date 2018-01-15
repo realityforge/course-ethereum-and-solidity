26a053f885b9a259d74a13ffa65315fbb3adbfb1
Peter Donald <peter@realityforge.org>
2018-01-15 19:12:51 +1100

    Extract output from compiling smart contract

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,3 +1,12 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+
+//Web3 module returns a constructor function.
+// We then use the constructor to create instance and connect it to specific networks
 const Web3 = require('web3');
+
+// And here's that instance
+const web3 = new Web3(ganache.provider());
+
+// Destructure the compiled output of the smart contract
+const { interface, bytecode } = require('../compile');
