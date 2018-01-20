42b220b7154f765ed7abb66b8d8d2261af99b01f
Peter Donald <peter@realityforge.org>
2018-01-20 18:59:36 +1100

    Style

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
