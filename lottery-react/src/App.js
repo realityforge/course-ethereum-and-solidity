d7ad724efc85515e7c894611c08f91a159444dcd
Peter Donald <peter@realityforge.org>
2018-01-20 19:27:31 +1100

    Start to pull data off contract and render into the screen

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
-import logo from './logo.svg';
-import web3 from './web3';
+import lottery from './lottery';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { manager: null };
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}.
         </p>
       </div>
     );
