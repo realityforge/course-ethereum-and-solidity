19b3857531b274d468fd3e1777dc0b9460721ab8
Peter Donald <peter@realityforge.org>
2018-01-20 19:38:25 +1100

    Start to write out some of the contract details

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 import lottery from './lottery';
+import web3 from './web3';
 
 class App extends Component {
-  state = { manager: '' };
+  state = { manager: '', entries: [], balance: '' };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const entries = await lottery.methods.getEntries().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, entries, balance });
   }
 
   render() {
@@ -15,7 +18,9 @@ class App extends Component {
       <div>
         <h2>Lottery Contract</h2>
         <p>
-          This contract is managed by {this.state.manager}.
+          This contract is managed by {this.state.manager}. There are
+          currently {this.state.entries.length} people entered, competing
+          to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
       </div>
     );
