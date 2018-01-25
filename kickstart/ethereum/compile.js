817d5bd8cea779484c31cb3b4b258a2efa229d32
Peter Donald <peter@realityforge.org>
2018-01-25 23:12:29 +1100

    Add script to compile contracts and output them in a build directory

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+
+// Delete the build folder
+fs.removeSync(buildPath);
+
+// Compile the source file
+const sourcePath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(sourcePath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+// Ensure build folder exists
+fs.ensureDirSync(buildPath);
+
+// write out separate json for each contract in source file
+for (let contract in output) {
+  const filename = path.resolve(buildPath, contract.replace(':', '') + '.json');
+  const contractData = output[contract];
+  fs.outputJsonSync(filename, contractData);
+}
