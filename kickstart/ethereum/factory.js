1443013a4f035cc3cfd741a75cbb9a9d8625c02c
Peter Donald <peter@realityforge.org>
2018-01-26 22:28:19 +1100

    Add file that setups up contract proxy

---
+++ kickstart/ethereum/factory.js
@@ -0,0 +1,8 @@
+import web3 from 'web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+// On rinkeby network, this was the address that we deployed to.
+const contractAddress = '0x17BaEbCf5213040cb4645242C6f95b3230055224';
+
+// Create proxy wrapper
+export default new web3.eth.Contract(JSON.parse(CampaignFactory.interface), contractAddress);
