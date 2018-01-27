ab004b1a1b1ef074c4c527001b64c8a1edaaa58a
Peter Donald <peter@realityforge.org>
2018-01-27 22:31:26 +1100

    Ugly hack to add css to page

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -11,6 +11,7 @@ class CampaignIndex extends React.Component {
   render() {
     return (
       <div>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
         {this.renderCampaigns()}
       </div>
     );
