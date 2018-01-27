99f6f4401eff8dc723269857d54188ea895a5853
Peter Donald <peter@realityforge.org>
2018-01-27 12:43:39 +1100

    Make sure is compatible with next.js serverside environment

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,6 +1,14 @@
 import Web3 from 'web3';
 
-// Initialize Web3 instanced based on provider that comes from MetaMask
-let web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // In a browser and Metamask is present
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // Not in the browser or Metamask not running. i.e. Server-side or in normal users browser
+  const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/p1UcQz0BDrRdDCymn0Pm');
+  web3 = new Web3(provider);
+}
 
 export default web3;
