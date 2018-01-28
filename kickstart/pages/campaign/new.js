b803e9f12c36739322dbe2630d7a0d2d12b066b8
Peter Donald <peter@realityforge.org>
2018-01-28 11:06:57 +1100

    Add minimal UI for creation

--- kickstart/pages/campaign/new.js
+++ kickstart/pages/campaign/new.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
+import {Button, Form, Input, Message} from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends React.Component {
+  state = {
+    minimumContribution: '',
+    errorMessage: '',
+    loading: false
+  };
+
   render() {
     return (
       <Layout>
-        <h3>New Campaign</h3>
+        <h3>Create a Campaign</h3>
+
+        <Form error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event => this.setState({ minimumContribution: event.target.value })}/>
+          </Form.Field>
+
+          <Message error header="Oops!" content={this.state.errorMessage}/>
+          <Button loading={this.state.loading} primary>
+            Create!
+          </Button>
+        </Form>
       </Layout>
     );
   }
