5ca469ea963b7ee6b0d746867e8d9eb086adf9e9
Peter Donald <peter@realityforge.org>
2018-01-28 11:01:16 +1100

    Move css into head of layout so avaialble whenever layout iused

--- kickstart/components/Layout.js
+++ kickstart/components/Layout.js
@@ -1,10 +1,16 @@
+import Head from 'next/head';
 import React from 'react';
-import {Container, Head} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
 import Header from './Header';
 
 export default props => {
   return (
     <Container>
+      {/* Head is a special Next.js component that portals the contained content into head for page*/}
+      <Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+      </Head>
+
       <Header/>
       {props.children}
     </Container>
