1f9369da1ab4141816421987e38debc8d891d1e1
Peter Donald <peter@realityforge.org>
2018-01-28 13:37:16 +1100

    Add minimal show

---
+++ kickstart/pages/campaign/show.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Layout from '../../components/Layout';
+
+class CampaignShow extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <h3>Campaign</h3>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignShow;
