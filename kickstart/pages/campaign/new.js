cfdbb2b7f097921871a24f9edb8bfb4c10680b90
Peter Donald <peter@realityforge.org>
2018-01-28 11:01:38 +1100

    Mock out new camapign page

---
+++ kickstart/pages/campaign/new.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Layout from '../../components/Layout';
+
+class CampaignNew extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <h3>New Campaign</h3>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignNew;
