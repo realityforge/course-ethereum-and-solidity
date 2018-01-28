12a32c679ba12ec57be79640e13979d98a88e18c
Peter Donald <peter@realityforge.org>
2018-01-28 19:32:15 +1100

    Redeploy contract

--- kickstart/ethereum/README.md
+++
@@ -1,3 +0,0 @@
-# Deploy Results
-
-    Contract deployed to 0x17BaEbCf5213040cb4645242C6f95b3230055224
--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 // On rinkeby network, this was the address that we deployed to.
-const contractAddress = '0x17BaEbCf5213040cb4645242C6f95b3230055224';
+const contractAddress = '0xcCD03E7008b4e5B39627B12C0574F022AF3e2c31';
 
 // Create proxy wrapper
 export default new web3.eth.Contract(JSON.parse(CampaignFactory.interface), contractAddress);
