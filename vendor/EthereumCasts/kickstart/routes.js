747caa1082b6e71d2968fab17204bc400157c66a
Peter Donald <peter@realityforge.org>
2018-01-28 17:53:00 +1100

    Braid: Update mirror 'vendor/EthereumCasts' to '30f96bc'

--- vendor/EthereumCasts/kickstart/routes.js
+++ vendor/EthereumCasts/kickstart/routes.js
@@ -3,6 +3,8 @@ const routes = NextRoutes();
 
 routes
   .add('/campaigns/new', '/campaigns/new')
-  .add('/campaigns/:address', '/campaigns/show');
+  .add('/campaigns/:address', '/campaigns/show')
+  .add('/campaigns/:address/requests', '/campaigns/requests/index')
+  .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 
 module.exports = routes;
