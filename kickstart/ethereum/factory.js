67d6d930795c305f08e339f0267a02a5ee82e7f9
Peter Donald <peter@realityforge.org>
2018-01-29 07:35:30 +1100

    Redeploy contract

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 // On rinkeby network, this was the address that we deployed to.
-const contractAddress = '0xcCD03E7008b4e5B39627B12C0574F022AF3e2c31';
+const contractAddress = '0x9136f3898Fb944e42592D08a5e127eb37cA0e2aE';
 
 // Create proxy wrapper
 export default new web3.eth.Contract(JSON.parse(CampaignFactory.interface), contractAddress);
