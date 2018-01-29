cb64d23663d901c784c4cf79f0318b273c34fdbf
Peter Donald <peter@realityforge.org>
2018-01-29 21:51:25 +1100

    Add routes for requests pages

--- kickstart/routes.js
+++ kickstart/routes.js
@@ -2,6 +2,8 @@ const routes = require('next-routes')();
 
 routes
 .add('/campaign/new', '/campaign/new')
-.add('/campaign/:address', '/campaign/show');
+.add('/campaign/:address', '/campaign/show')
+.add('/campaign/:address/requests', '/campaign/requests/index')
+.add('/campaign/:address/requests/new', '/campaign/requests/new');
 
 module.exports = routes;
